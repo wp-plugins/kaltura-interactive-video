a1b6c53ae13e51af9a621cddd0df9618d68f11b1
kaltura <kaltura@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-07-31 12:54:51 +0000

    git-svn-id: https://plugins.svn.wordpress.org/kaltura-interactive-video/tags/0.9.5@57216 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ interactive_video/ajax_update_kshow_name.php
@@ -0,0 +1,18 @@
+<?php 
+	define('WP_USE_THEMES', false);
+	require('../../../wp-blog-header.php');
+	require_once('settings.php');
+	require_once('lib/common.php');
+	require_once('lib/kaltura_helpers.php');
+	require_once("lib/kaltura_model.php");
+	
+	$sessionUser = kalturaGetSessionUser();
+	
+	$kshowId = @$_GET["kshowId"];
+	
+	// update the kshow
+	$kshowUpdate = new KalturaKShow();
+	$kshowUpdate->name = "My Interactive Video";
+	$kalturaClient = getKalturaClient(false, "edit:".$kshowId);  
+	KalturaModel::updateKshow($kalturaClient, $kshowId, $kshowUpdate);
+?>
