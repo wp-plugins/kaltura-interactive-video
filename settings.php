c1e59ccb4907123f8afe1c9cf564e03c88c9dd97
kaltura <kaltura@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-08-13 11:41:31 +0000

    git-svn-id: https://plugins.svn.wordpress.org/kaltura-interactive-video/trunk@59471 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- settings.php
+++ settings.php
@@ -1,13 +1,10 @@
 <?php
-	class KalturaWPSettings
-	{
-		const SERVER_URL = "http://www.kaltura.com";
-		const UICONF_ID = 500;
-		const BASE_WIDGET_ID = 500;
-		const ANONYMOUS_USER_ID = "Anonymous";
-		const KSE_UICONF = 502;
-		const KCW_UICONF = 501;
-		const KCW_UICONF_COMMENTS = 503;
-		const KCW_UICONF_FOR_SE = 504;
-	} 
+	define("KALTURA_SERVER_URL", "http://www.kaltura.com");
+	define("KALTURA_UICONF_ID", 500);
+	define("KALTURA_BASE_WIDGET_ID", 500);
+	define("KALTURA_ANONYMOUS_USER_ID", "Anonymous");
+	define("KALTURA_KSE_UICONF", 502);
+	define("KALTURA_KCW_UICONF", 501);
+	define("KALTURA_KCW_UICONF_COMMENTS", 503);
+	define("KALTURA_KCW_UICONF_FOR_SE", 504);
 ?>
