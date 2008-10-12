8a0729852e1128a0f33b14df633cd59fad3267c2
kaltura <kaltura@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-10-12 08:46:41 +0000

    git-svn-id: https://plugins.svn.wordpress.org/kaltura-interactive-video/trunk@68653 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- view/view_deleted.php
+++ view/view_deleted.php
@@ -1,8 +1,11 @@
-<div class="updated">
-	<p>
-		The video was deleted successfully.
-	</p>
+<div class="wrap kalturaWrapFix">
+	<div class="updated">
+		<p>
+			The video was deleted successfully.
+		</p>
+	</div>
 </div>
-<div class="wrap">
+
+<div class="wrap kalturaWrapFix">
 	<a href="<?php echo $viewData["redirectUrl"]; ?>">Click here if you are not redirected automatically.</a>
 </div>
