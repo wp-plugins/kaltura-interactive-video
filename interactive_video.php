ff899446eb862b526013204111fc344f22c8e274
kaltura <kaltura@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-10-13 11:10:14 +0000

    git-svn-id: https://plugins.svn.wordpress.org/kaltura-interactive-video/trunk@68787 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- interactive_video.php
+++ interactive_video.php
@@ -3,7 +3,7 @@
 Plugin Name: Interactive Video
 Plugin URI: http://kaltura.org/community/viewtopic.php?f=4&t=3
 Description: Add interactive video capabilities to your blog! Enhance your WordPress blog with a full video experience. Enable playing and editing of video remixes.
-Version: 1.1
+Version: 1.15
 Author: Kaltura
 Author URI: http://corp.kaltura.com
 */
